326202313c8fd58a58869797473ef805ee186815
Rishabh Gupta <belikerishabh@gmail.com>
2025-01-17 05:05:27 +0530

    remove dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist/cosmos',
     emptyOutDir: true,
     rollupOptions: {
       input: {
